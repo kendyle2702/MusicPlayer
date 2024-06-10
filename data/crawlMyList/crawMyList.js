783df2bf5b4b931d0988d640db1965ee922132d5
Kendy Le <101160086+kendyle2702@users.noreply.github.com>
2024-06-10 14:28:58 +0700

    Update crawMyList.js

--- data/crawlMyList/crawMyList.js
+++ data/crawlMyList/crawMyList.js
@@ -19,93 +19,94 @@ const myApi = "https://music-player-server-lime.vercel.app";
 //     id: "Z6UFF9Z6",
 //     name: "để tôi ôm em bằng giai điệu này",
 //   },
+// {
+//     id: "ZW67FWWF",
+//     name: "Bức Tranh Từ Màu Nước Mắt",
+//   },
+//   {
+//     id: "ZZAIAIAZ",
+//     name: "SAIGON SIMPLE LOVE",
+//   },
+//   {
+//     id: "ZW6DF66B",
+//     name: "Day Dứt Nỗi Đau",
+//   },
+//   {
+//     id: "ZW7WDBZA",
+//     name: "Dưới Những Cơn Mưa",
+//   },
+//   {
+//     id: "ZOZA0U99",
+//     name: "Không Muốn Yêu Lại Càng Say Đắm",
+//   },
+//   {
+//     id: "ZZAAZBZE",
+//     name: "Có Em",
+//   },
+//   {
+//     id: "Z67CZDWA",
+//     name: "Không Yêu Xin Đừng Nói",
+//   },
+  
+ 
+//   {
+//     id: "Z6UBADAF",
+//     name: "Yêu Người Có Ước Mơ",
+//   },
+//   {
+//     id: "ZOU7OB7I",
+//     name: "Bao Tiền Một Mớ Bình Yên?",
+//   },
+//   {
+//     id: "ZW9CFDU9",
+//     name: "Suýt Nữa Thì (Chuyến Đi Của Thanh Xuân OST)",
+//   },
+//   {
+//     id: "ZW8I76Z8",
+//     name: "1 Phút",
+//   },
+//   {
+//     id: "ZZB8UW0E",
+//     name: "Như Anh Đã Thấy Em",
+//   },
+//   {
+//     id: "ZWAIDIEC",
+//     name: "Mình Dành Cho Nhau Nỗi Buồn",
+//   },
+//   {
+//     id: "ZWDZCE80",
+//     name: "Bông Hoa Đẹp Nhất",
+//   },
+//   {
+//     id: "ZW8IC98I",
+//     name: "Ta Còn Yêu Nhau",
+//   },
+
+
+//   {
+//     id: "ZZEF769O",
+//     name: "Ngày Mai Em Đi Mất",
+//   },
+//   {
+//     id: "ZZDB9868",
+//     name: "Chạy Khỏi Thế Giới Này",
+//   },
+ 
+
+//   {
+//     id: "ZW8WOA0A",
+//     name: "Về Phía Mưa",
+//   },
+//   {
+//     id: "ZW7F90DU",
+//     name: "Cafe, Thuốc Lá & Những Ngày Vui",
+//   },
 const idMusics = [
   {
     id: "ZWBIEWBI",
     name: "Đường Tôi Chở Em Về",
   },
-  {
-    id: "ZW67FWWF",
-    name: "Bức Tranh Từ Màu Nước Mắt",
-  },
-  {
-    id: "ZZAIAIAZ",
-    name: "SAIGON SIMPLE LOVE",
-  },
-  {
-    id: "ZW6DF66B",
-    name: "Day Dứt Nỗi Đau",
-  },
-  {
-    id: "ZW7WDBZA",
-    name: "Dưới Những Cơn Mưa",
-  },
-  {
-    id: "ZOZA0U99",
-    name: "Không Muốn Yêu Lại Càng Say Đắm",
-  },
-  {
-    id: "ZZAAZBZE",
-    name: "Có Em",
-  },
-  {
-    id: "Z67CZDWA",
-    name: "Không Yêu Xin Đừng Nói",
-  },
   
- 
-  {
-    id: "Z6UBADAF",
-    name: "Yêu Người Có Ước Mơ",
-  },
-  {
-    id: "ZOU7OB7I",
-    name: "Bao Tiền Một Mớ Bình Yên?",
-  },
-  {
-    id: "ZW9CFDU9",
-    name: "Suýt Nữa Thì (Chuyến Đi Của Thanh Xuân OST)",
-  },
-  {
-    id: "ZW8I76Z8",
-    name: "1 Phút",
-  },
-  {
-    id: "ZZB8UW0E",
-    name: "Như Anh Đã Thấy Em",
-  },
-  {
-    id: "ZWAIDIEC",
-    name: "Mình Dành Cho Nhau Nỗi Buồn",
-  },
-  {
-    id: "ZWDZCE80",
-    name: "Bông Hoa Đẹp Nhất",
-  },
-  {
-    id: "ZW8IC98I",
-    name: "Ta Còn Yêu Nhau",
-  },
-
-
-  {
-    id: "ZZEF769O",
-    name: "Ngày Mai Em Đi Mất",
-  },
-  {
-    id: "ZZDB9868",
-    name: "Chạy Khỏi Thế Giới Này",
-  },
- 
-
-  {
-    id: "ZW8WOA0A",
-    name: "Về Phía Mưa",
-  },
-  {
-    id: "ZW7F90DU",
-    name: "Cafe, Thuốc Lá & Những Ngày Vui",
-  },
 ];
 
 const crawMyList = () => {
